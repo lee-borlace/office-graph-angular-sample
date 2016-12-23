df5a64954e53a0dcabbe3faffe486dc7aaffba84
leeroy79 <be5525c067f1e25902a584bd001aa116>
2016-12-23 11:53:47 +0800

    Initial add

---
+++ public/scripts/config.js
@@ -0,0 +1,8 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+var clientId = "d292c339-aceb-4ded-8f3f-bb03fc9abdcd";
+var redirectUrl = "http://localhost:8080/login";
+var graphScopes = "user.read mail.send";
